b5602738408fbd0f9dd2fc62b3478f0e2f389f78
Mauro Balades <mauro.balades@tutanota.com>
2024-08-15 22:07:14 +0200

    chore: Rebuild themes.json after theme submission

---
+++ scripts/rebuild-themes.py
@@ -0,0 +1,27 @@
+
+import os
+import json
+
+THEMES_FOLDER = './themes'
+THEMES_DATA_FILE = './themes.json'
+
+def main():
+    for theme in os.listdir(THEMES_FOLDER):
+        theme_folder = os.path.join(THEMES_FOLDER, theme)
+        if not os.path.isdir(theme_folder):
+            continue
+        theme_data_file = os.path.join(theme_folder, 'theme.json')
+        if not os.path.exists(theme_data_file):
+            continue
+        with open(theme_data_file, 'r') as f:
+            theme_data = json.load(f)
+            with open(THEMES_DATA_FILE, 'r') as f:
+                themes_data = json.load(f)
+                themes_data[theme] = theme_data
+                with open(THEMES_DATA_FILE, 'w') as f:
+                    json.dump(themes_data, f, indent=4)
+        print(f"Rebuilt theme: {theme}")
+    print("Rebuilt all themes!")
+  
+if __name__ == '__main__':
+    main()
