079d43b32a6fb4af7e581cc3c5961d17be3de1ba
mauro 🤙 <91018726+mauro-balades@users.noreply.github.com>
2024-08-25 12:29:46 +0200

    Update _ensure_packages_have_version.py

--- scripts/_ensure_packages_have_version.py
+++ scripts/_ensure_packages_have_version.py
@@ -1,6 +1,6 @@
 
 def ensure_packages_have_version(theme_data):
-    if 'version' not in theme_data:
+    if theme_data.get('version', None) is None:
         print("  Version is required in theme.json. Adding default value of 1.0.0.")
         theme_data['version'] = '1.0.0'
     return theme_data
